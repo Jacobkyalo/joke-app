c4c110c6c1c3e89a30a37c7f6dfc6685afeff42c
Jacob <jacobkyalo43@gmail.com>
2022-06-25 17:11:07 +0300

    added functionality to the app

--- src/App.js
+++ src/App.js
@@ -4,28 +4,10 @@ import Header from "./components/Header";
 import JokeCard from "./components/JokeCard";
 
 function App() {
-  const [jokes, setJokes] = useState([]);
-
-  useEffect(() => {
-    fetchJokes();
-  }, []);
-
-  const fetchJokes = async () => {
-    const response = await fetch("https://icanhazdadjoke.com/slack");
-    const jsonData = await response.json();
-    setJokes(jsonData.attachments[0].text);
-  };
   return (
     <div className="app">
       <Header />
       <JokeCard />
-      <button
-        type="button"
-        className="joke-btn"
-        onClick={() => console.log("Generated!")}
-      >
-        Generate Joke
-      </button>
     </div>
   );
 }
--- src/components/JokeCard.js
+++ src/components/JokeCard.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import laugh from "../laughing.jfif";
 
 const JokeCard = () => {
+  const [joke, setJoke] = useState([]);
+
+  useEffect(() => {
+    fetchJokes();
+  }, []);
+
+  const fetchJokes = async () => {
+    const headers = {
+      headers: {
+        Accept: "application/json",
+      },
+    };
+    const response = await fetch("https://icanhazdadjoke.com", headers);
+    const jsonData = await response.json();
+    setJoke(jsonData.joke);
+  };
   return (
     <div className="container">
       <div className="joke-card">
         <img src={laugh} alt="laugh-emoji" className="img" />
-        <p className="joke-text">A random joke</p>
+        <p className="joke-text">{joke}</p>
+        <button type="button" className="joke-btn" onClick={fetchJokes}>
+          Generate Joke
+        </button>
       </div>
     </div>
   );
